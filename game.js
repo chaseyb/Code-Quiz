67c08d6c363356e5e7cb52ca5bb53199f358942c
Chase Johnson <Chase.b.johnson@gmail.com>
2020-04-24 14:20:58 -0700

    Updated HTML & CSS

---
+++ game.js
@@ -0,0 +1,20 @@
+// Variable for questions, an array of objects 
+
+var questions = [{
+    question: "What is the state tree of North Carolina?",
+    answers: ["Dogwood", "Longleaf Pine", "Bald Cypress","Fraser Fir"],
+    correctAnswer: "Dogwood",
+    image: "assets/images/imagename"
+}, {
+    question: "North Carolina grows the most what in the nation?",
+    answers: ["Tomatoes", "Sweet Potatoes", "Broccoli", "Soybeans"],
+    correctAnswer: "Sweet Potatoes",
+    image: "assets/images/imagename",
+}, {
+question: "What is the state bird?",
+    answers: ["Bald Eagle", "Blue Jay", "Cardinal", "Wild Turkey"],
+    correctAnswer: "Cardinal",
+    image: "assets/images/imagename",
+},
+
+];
